93dbc517b622480f04de202e53bcffd369b9aa7f
jakewmiles <jake.w.miles@gmail.com>
2021-10-21 13:59:10 +0100

    feat: grab scraped data and feed into struct

--- main.go
+++ main.go
@@ -19,7 +19,23 @@ func main() {
 	c := colly.NewCollector()
 
 	c.OnHTML("tbody > tr", func(e *colly.HTMLElement) {
+		symbol := e.ChildText(".d-lg-none")
+		market_cap := e.ChildText(".td-market_cap")
+		price := e.ChildText(".td-price")
+		volume_24h := e.ChildText(".td-liquidity_score")
+		percent_change_1h := e.ChildText(".td-change1h")
+		percent_change_24h := e.ChildText(".td-change24h")
+		percent_change_7d := e.ChildText(".td-change7d")
 
+		coin := Coin{
+			Symbol:           symbol,
+			MarketCap:        market_cap,
+			Price:            price,
+			Volume24H:        volume_24h,
+			PercentChange1H:  percent_change_1h,
+			PercentChange24H: percent_change_24h,
+			PercentChange7D:  percent_change_7d,
+		}
 	})
 
 	c.Visit("https://www.coingecko.com/en")
