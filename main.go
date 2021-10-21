8c1f4ab642f03fd92e450a5009019e22bb79ac81
jakewmiles <jake.w.miles@gmail.com>
2021-10-21 13:54:06 +0100

    feat: install colly and create coin struct

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+type Coin struct {
+	Symbol           string
+	MarketCap        string
+	Price            string
+	Volume24H        string
+	PercentChange1H  string
+	PercentChange24H string
+	PercentChange7D  string
+}
+
+func main() {
+
+}
